44b2c98140f9afdfae2a06061f95afe0f0d2d1a1
complete516 <chenggeng516@163.com>
2021-03-03 18:59:23 +0800

    删除错误代码

--- assets/Scripts/Mgr/UIMgr.ts
+++ assets/Scripts/Mgr/UIMgr.ts
@@ -2,10 +2,6 @@
 import { _decorator, Component, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
-export UIType {
-    
-}
-
 
 @ccclass('UIMgrTs')
 export class UIMgrTs extends Component {

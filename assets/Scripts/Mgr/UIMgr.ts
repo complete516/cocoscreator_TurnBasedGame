e3a917c95cf37138439eb421828c8e818656fc9d
complete516 <chenggeng516@163.com>
2021-03-05 15:37:28 +0800

    修改错误代码，修复报错

--- assets/Scripts/Mgr/UIMgr.ts
+++ assets/Scripts/Mgr/UIMgr.ts
@@ -3,9 +3,19 @@ import { _decorator, Component, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 
-@ccclass('UIMgrTs')
-export class UIMgrTs extends Component {
-    
+@ccclass('UIMgr')
+export class UIMgr extends Component {
+
+    private static instance: UIMgr | null = null;
+
+    private pop: Node[] = [];
+
+    public static get Instance() {
+        if (UIMgr.instance == null) {
+            UIMgr.instance = new UIMgr();
+        }
+        return UIMgr.instance;
+    }
 }
 
 
